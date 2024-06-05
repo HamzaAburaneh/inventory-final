728659efd5b765c81eeec3df899e86ac876037e8
Hamza Aburaneh <32944558+HamzaAburaneh@users.noreply.github.com>
2024-06-05 19:09:51 -0400

    feat: Add theme toggle and navbar components to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,21 @@
-<!-- src/routes/+layout.svelte -->
 <script>
 	import ThemeToggle from '../components/ThemeToggle.svelte';
+	import Navbar from '../components/Navbar.svelte';
 	import '../styles/global.css';
+	import { onMount } from 'svelte';
+	import { theme } from '../themes';
+	import { get } from 'svelte/store';
+
+	let currentTheme = get(theme);
+
+	onMount(() => {
+		if (typeof document !== 'undefined') {
+			document.documentElement.setAttribute('data-theme', currentTheme);
+		}
+	});
 </script>
+
+<Navbar />
 
 <main>
 	<ThemeToggle />
