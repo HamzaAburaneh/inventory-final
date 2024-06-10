60c680ec6497a281e0c078f3ef98a6b552e238ad
Hamza Aburaneh <32944558+HamzaAburaneh@users.noreply.github.com>
2024-06-09 22:21:39 -0400

    Simplify Theme Handling in Main Layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,24 +1,11 @@
 <script>
-	import ThemeToggle from '../components/ThemeToggle.svelte';
 	import Navbar from '../components/Navbar.svelte';
 	import '../styles/global.css';
-	import { onMount } from 'svelte';
-	import { theme } from '../themes';
-	import { get } from 'svelte/store';
-
-	let currentTheme = get(theme);
-
-	onMount(() => {
-		if (typeof document !== 'undefined') {
-			document.documentElement.setAttribute('data-theme', currentTheme);
-		}
-	});
 </script>
 
 <Navbar />
 
 <main>
-	<ThemeToggle />
 	<slot />
 </main>
 
