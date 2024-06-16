e0d6a2f280bf7d5119c5efd1f7e4821d6be2cd1d
Hamza Aburaneh <32944558+HamzaAburaneh@users.noreply.github.com>
2024-06-16 13:42:48 -0400

    Styling

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -8,13 +8,13 @@
 	}
 </script>
 
-<nav class="bg-gray-800 p-4">
+<nav class="p-4" style="background-color: var(--nav-color);">
 	<div class="container mx-auto flex justify-between items-center">
 		<a href="/" class="text-white text-xl">Brand</a>
 		<button class="text-white md:hidden" on:click={toggleMenu}> ☰ </button>
 		<ul class="hidden md:flex space-x-4">
 			<li><a href="/" class="text-white">Home</a></li>
-			<li><a href="/about" class="text-white">About</a></li>
+			<li><a href="/manageItems" class="text-white">Item Manager</a></li>
 			<li><a href="/contact" class="text-white">Contact</a></li>
 			<li><ThemeToggle /></li>
 		</ul>
