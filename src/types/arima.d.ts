d95c35f277ec785a9def273858b53a0955bc96b5
Hamza Aburaneh <32944558+HamzaAburaneh@users.noreply.github.com>
2025-07-14 22:56:27 -0400

    delete typescript files

--- src/types/arima.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'arima' {
-	export class ARIMA {
-		constructor(options: { p: number; d: number; q: number; verbose?: boolean });
-
-		train(data: number[]): void;
-		predict(steps: number): [number[], number[]];
-	}
-}
